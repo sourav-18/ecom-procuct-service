11dcdc2d620d10bf71e6e0f3e87e0e5f8a185a6d
sourav-18 <souravhd2@gmail.com>
2026-04-22 08:20:44 +0530

    dd

--- src/main/java/com/ms/product_service/Filter/IpFilter.java
+++ src/main/java/com/ms/product_service/Filter/IpFilter.java
@@ -4,17 +4,31 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 @Component
+@Profile("prod")
 public class IpFilter extends OncePerRequestFilter {
+
+    @Value("${server.allow.id}")
+    private String ALLOW_IP;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String[] ips = ALLOW_IP.split(",");
         String clientIp = request.getRemoteAddr();
+        System.out.println(ALLOW_IP);
         System.out.println(clientIp);
+        if (Arrays.stream(ips).noneMatch(item->item.equals(clientIp))) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Forbidden");
+            return;
+        }
         filterChain.doFilter(request,response);
     }
 }
